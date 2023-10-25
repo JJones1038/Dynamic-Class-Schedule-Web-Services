8bfd832c2851bd64e8aa29c278984ce47c2f4e47
JJones1038 <67795967+JJones1038@users.noreply.github.com>
2023-10-24 22:16:06 -0500

    Registration/Search Updates. Added PersonnelDatabase, Reg.,Srch DAO

--- src/java/edu/jsu/mcis/cs425/ex2/PersonnelServlet.java
+++ src/java/edu/jsu/mcis/cs425/ex2/PersonnelServlet.java
@@ -1,56 +1,53 @@
-//package edu.jsu.mcis.cs425.ex2;
-//
-//import java.io.PrintWriter;
-//import jakarta.servlet.ServletContext;
-//import jakarta.servlet.http.HttpServlet;
-//import jakarta.servlet.http.HttpServletRequest;
-//import jakarta.servlet.http.HttpServletResponse;
-//import edu.jsu.mcis.cs425.ex2.dao.*;
-//
-//public class PersonnelServlet extends HttpServlet {
-//
-//    @Override
-//    protected void doGet(HttpServletRequest request, HttpServletResponse response) {
-//
-//        DAOFactory daoFactory = null;
-//
-//        ServletContext context = request.getServletContext();
-//
-//        if (context.getAttribute("daoFactory") == null) {
-//            System.err.println("*** Creating New DAOFactory Instance ...");
-//            daoFactory = new DAOFactory();
-//            context.setAttribute("daoFactory", daoFactory);
-//        }
-//        else {
-//            daoFactory = (DAOFactory) context.getAttribute("daoFactory");
-//        }
-//
-//        response.setContentType("text/html;charset=UTF-8");
-//
-//        try ( PrintWriter out = response.getWriter()) {
-//
-//            String lastname = request.getParameter("search");
-//            out.println("<p>Search Parameter: " + lastname + "</p>");
-//            
-//            PersonnelDAO dao = daoFactory.getPersonnelDAO();
-//
-//            out.println(dao.find(lastname));
-//
-//        }
-//        catch (Exception e) {
-//            e.printStackTrace();
-//        }
-//
-//    }
-//
-//    @Override
-//    protected void doPost(HttpServletRequest request, HttpServletResponse response) {
-//        doGet(request, response);
-//    }
-//
-//    @Override
-//    public String getServletInfo() {
-//        return "Personnel Servlet";
-//    }
-//
-//}
+package edu.jsu.mcis.cs425.ex2;
+
+import java.io.PrintWriter;
+import jakarta.servlet.ServletContext;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+public class PersonnelServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) {
+
+        PersonnelDatabase db = null;
+
+        ServletContext context = request.getServletContext();
+
+        if (context.getAttribute("db") == null) {
+            System.err.println("*** Creating New PersonnelDatabase Instance ...");
+            db = new PersonnelDatabase();
+            context.setAttribute("db", db);
+        }
+        else {
+            db = (PersonnelDatabase) context.getAttribute("db");
+        }
+
+        response.setContentType("text/html;charset=UTF-8");
+
+        try ( PrintWriter out = response.getWriter()) {
+
+            String lastname = request.getParameter("search");
+
+            out.println("<p>Search Parameter: " + lastname + "</p>");
+            out.println(db.getPersonnelAsHTML(lastname));
+
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) {
+        doGet(request, response);
+    }
+
+    @Override
+    public String getServletInfo() {
+        return "Test Servlet";
+    }
+
+}
--- src/java/edu/jsu/mcis/cs425/ex2/dao/DAOFactory.java
+++ src/java/edu/jsu/mcis/cs425/ex2/dao/DAOFactory.java
@@ -5,55 +5,50 @@ import javax.naming.Context;
 import javax.naming.InitialContext;
 import javax.sql.DataSource;
 
-public final class DAOFactory {
-    
-    private static final String PREFIX = "ex2";
-    private static final String PROPERTY_ENVCONTEXT = "envcontext";
-    private static final String PROPERTY_INITCONTEXT = "initcontext";
-    
-    private Connection conn;
-    
+public class DAOFactory {
+
+    private DataSource ds = null;
+
     public DAOFactory() {
 
-        DAOProperties properties = new DAOProperties(PREFIX);
-
-        String envcontext = properties.getProperty(PROPERTY_ENVCONTEXT);
-        String initcontext = properties.getProperty(PROPERTY_INITCONTEXT);
-        
         try {
+
             Context envContext = new InitialContext();
-            Context initContext = (Context) envContext.lookup(envcontext);
-            DataSource ds = (DataSource) initContext.lookup(initcontext);
-            if (ds != null) {
-                this.conn = ds.getConnection();
-            }
+            Context initContext = (Context) envContext.lookup("java:/comp/env");
+            ds = (DataSource) initContext.lookup("jdbc/db_pool");
+
         }
         catch (Exception e) {
-            conn = null;
             e.printStackTrace();
         }
-        
+
     }
-    
+
     Connection getConnection() {
-        return conn;
-    }
-    
-    public boolean isClosed() {
-        
-        boolean isClosed = true;
-        
+
+        Connection c = null;
+
         try {
-            isClosed = conn.isClosed();
+
+            if (ds != null) {
+                c = ds.getConnection();
+            }
+
         }
-        catch (Exception e) { e.printStackTrace(); }
-        
-        return isClosed;
-        
+        catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return c;
+
     }
     
-    public PersonnelDAO getPersonnelDAO() {
-        return new PersonnelDAO(this);
+    public RegistrationDAO getRegistrationDAO() {
+        return new RegistrationDAO(this);
+    }
+    
+    public SearchDAO getSearchDAO() {
+        return new SearchDAO(this);
     }
     
 }
